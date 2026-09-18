9f6c35022c9b281efb065874c4adaf801cf11a43
Roberto F. Tercero <rftercero1@gmail.com>
2026-09-18 14:10:55 -0600

    feat(ui): vista de vectores y evaluador visual de combinacion lineal en Rn

--- prettycalc/ui/modules/__init__.py
+++ prettycalc/ui/modules/__init__.py
@@ -2,5 +2,6 @@
 
 from prettycalc.ui.modules.linear_systems_view import LinearSystemsView
 from prettycalc.ui.modules.matrix_operations_view import MatrixOperationsView
+from prettycalc.ui.modules.vectors_view import VectorsView
 
-__all__ = ["LinearSystemsView", "MatrixOperationsView"]
+__all__ = ["LinearSystemsView", "MatrixOperationsView", "VectorsView"]
